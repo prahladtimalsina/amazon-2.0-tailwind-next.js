ef47cb3a7e71f066a22733df7ca393af5ba1c1a6
prahladtimalsina <prahladtimalsina7@gmail.com>
2021-09-11 00:21:25 +0545

    config check

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@ module.exports = {
   publicRuntimeConfig: {
     APP_NAME: "Instagram",
     API_DEVELOPMENT: "http://localhost:8000/api",
-    API_PRODUCTION: "https://prahladtimalsina.com/api",
+    API_PRODUCTION: "https://prahladtimalsina.com.np/api",
     PRODUCTION: false,
     DOMAIN_DEVELOPMENT: "http://localhost:3000",
-    DOMAIN_PRODUCTION: "https://prahladtimalsina.com",
+    DOMAIN_PRODUCTION: "https://prahladtimalsina.com.np",
     FB_APP_ID: "415724632408061",
   },
   images: {
-    domains: ["localhost", "fakestoreapi.com",'links.papareact.com'],
+    domains: ["localhost", "fakestoreapi.com"],
   },
   env: {
     stripe_public_key: `${process.env.STRIPE_PUBLIC_KEY}`,
